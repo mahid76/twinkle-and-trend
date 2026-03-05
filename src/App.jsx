982cc34b30b6a11cd1be72f26907d292c4393baf
mahid76 <mahidulislamsohan76@gmail.com>
2026-03-06 01:21:18 +0600

    welcome

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 
 function App() {
 	return (
-		<body className="bg-amber-300 flex justify-center items-center" >
-			<div className="h-10 w-full text-red-500 bg-black text-center flex justify-center items-center">
-				<h1>hi</h1>
+		<body className="bg-amber-300 flex items-center justify-center h-screen  ">
+			<div className="">
+				<h1 className="text-7xl p-10 bg-blue-200">Welcome to Twinkle and Trend</h1>
 			</div>
 		</body>
 	);
