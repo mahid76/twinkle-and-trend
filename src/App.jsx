2ae6b7c286826d4ce71f2052350dee5785bb1cdb
mahid76 <mahidulislamsohan76@gmail.com>
2026-03-06 01:07:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<body className="bg-amber-300 flex justify-center items-center" >
+			<div className="h-10 w-full text-red-500 bg-black text-center flex justify-center items-center">
+				<h1>hi</h1>
+			</div>
+		</body>
+	);
+}
+
+export default App;
